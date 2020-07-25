e2186aa7cadc7883ff154d5d5a0c70eda59d0dad
Katsuyoshi Miura <k-miura@pesol-rd.co.jp>
2020-07-25 15:23:21 +0900

    indented

--- ngspiceRaw2Csv.c
+++ ngspiceRaw2Csv.c
@@ -37,9 +37,9 @@ void wordncpy(char *dest, char *src, int bufsize)
 void lowercase(char *str, int bufsize)
 {
 	int i;
-	for(i=0; i<bufsize-1 && str[i] != '\0'; i++)
-	{
-		if(str[i] >= 'A' && str[i] <= 'Z') str[i] = str[i] - ('A' - 'a');
+	for (i = 0; i < bufsize - 1 && str[i] != '\0'; i++) {
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] = str[i] - ('A' - 'a');
 	}
 }
 
@@ -55,7 +55,8 @@ int main(int argc, char **argv)
 	int *output_vars;
 
 	/* Convert arguments to lowercase */
-	for(i=1; i<argc; i++) lowercase(argv[i], LABEL_SIZE);
+	for (i = 1; i < argc; i++)
+		lowercase(argv[i], LABEL_SIZE);
 
 	/*
 	 * Header format
@@ -73,25 +74,25 @@ int main(int argc, char **argv)
 	 */
 
 	/* Title: * circuit description */
-	if(NULL == fgets(buf, BUFSIZE, stdin)) {
+	if (NULL == fgets(buf, BUFSIZE, stdin)) {
 		perror("Unexpected EOF or I/O error");
 		exit(1);
 	}
 
 	/* Date: Mon Jun 13 23:38:21  2016 */
-	if(NULL == fgets(buf, BUFSIZE, stdin)) {
+	if (NULL == fgets(buf, BUFSIZE, stdin)) {
 		perror("Unexpected EOF or I/O error");
 		exit(1);
 	}
 
 	/* Plotname: Transient Analysis */
-	if(NULL == fgets(buf, BUFSIZE, stdin)) {
+	if (NULL == fgets(buf, BUFSIZE, stdin)) {
 		perror("Unexpected EOF or I/O error");
 		exit(1);
 	}
 
 	/* Flags: real */
-	if(NULL == fgets(buf, BUFSIZE, stdin)) {
+	if (NULL == fgets(buf, BUFSIZE, stdin)) {
 		perror("Unexpected EOF or I/O error");
 		exit(1);
 	}
@@ -101,7 +102,7 @@ int main(int argc, char **argv)
 	}
 
 	/* No. Variables: 2 */
-	if(NULL == fgets(buf, BUFSIZE, stdin)) {
+	if (NULL == fgets(buf, BUFSIZE, stdin)) {
 		perror("Unexpected EOF or I/O error");
 		exit(1);
 	}
@@ -122,7 +123,7 @@ int main(int argc, char **argv)
 	}
 
 	/* No. Points: 1956 */
-	if(NULL == fgets(buf, BUFSIZE, stdin)) {
+	if (NULL == fgets(buf, BUFSIZE, stdin)) {
 		perror("Unexpected EOF or I/O error");
 		exit(1);
 	}
@@ -137,7 +138,7 @@ int main(int argc, char **argv)
 	}
 
 	/* Variables: */
-	if(NULL == fgets(buf, BUFSIZE, stdin)) {
+	if (NULL == fgets(buf, BUFSIZE, stdin)) {
 		perror("Unexpected EOF or I/O error");
 		exit(1);
 	}
@@ -145,10 +146,9 @@ int main(int argc, char **argv)
 		fprintf(stderr, "Format error: %s", buf);
 		exit(1);
 	}
-
 	// Read variable labels
 	for (i = 0; i < n_vars; i++) {
-		if(NULL == fgets(buf, BUFSIZE, stdin)) {
+		if (NULL == fgets(buf, BUFSIZE, stdin)) {
 			perror("Unexpected EOF or I/O error");
 			exit(1);
 		}
@@ -162,25 +162,31 @@ int main(int argc, char **argv)
 
 	/* Select output fields */
 	n_output_vars = n_vars;
-	if(argc > 1) n_output_vars = argc - 1;
+	if (argc > 1)
+		n_output_vars = argc - 1;
 	output_vars = (int *)calloc(n_output_vars, sizeof(int));
-	if(NULL == output_vars) {
+	if (NULL == output_vars) {
 		perror("memory allocation failed");
 		exit(1);
 	}
-	if(argc==1) {
+	if (argc == 1) {
 		/* All fields are printed (default). */
-		for(i=0; i<n_output_vars; i++) output_vars[i] = i;
+		for (i = 0; i < n_output_vars; i++)
+			output_vars[i] = i;
 	} else {
 		/* Only specified fields are printed. */
-		for(i=0; i<n_output_vars; i++) {
-			for(j=0; j<n_vars; j++) {
-				if(0 == strncmp(label + LABEL_SIZE * j, argv[i + 1], LABEL_SIZE)) break;
+		for (i = 0; i < n_output_vars; i++) {
+			for (j = 0; j < n_vars; j++) {
+				if (0 ==
+				    strncmp(label + LABEL_SIZE * j, argv[i + 1],
+					    LABEL_SIZE))
+					break;
 			}
-			if(j<n_vars) output_vars[i] = j;
+			if (j < n_vars)
+				output_vars[i] = j;
 			else {
 				fputs("unknown variable: ", stderr);
-				fputs(argv[i+1], stderr);
+				fputs(argv[i + 1], stderr);
 				fputc('\n', stderr);
 				output_vars[i] = -1;
 			}
@@ -188,10 +194,10 @@ int main(int argc, char **argv)
 	}
 
 	fputs("# ", stdout);
-	for(i=0; i < n_output_vars; i++) {
-		if(output_vars[i]<0) {
+	for (i = 0; i < n_output_vars; i++) {
+		if (output_vars[i] < 0) {
 			fputs("unknown:", stdout);
-			fputs(argv[i+1], stdout);
+			fputs(argv[i + 1], stdout);
 		} else {
 			fputs(label + LABEL_SIZE * output_vars[i], stdout);
 		}
@@ -203,7 +209,7 @@ int main(int argc, char **argv)
 	}
 
 	/* Binary: */
-	if(NULL == fgets(buf, BUFSIZE, stdin)) {
+	if (NULL == fgets(buf, BUFSIZE, stdin)) {
 		perror("Unexpected EOF or I/O error");
 		exit(1);
 	}
@@ -225,8 +231,9 @@ int main(int argc, char **argv)
 			}
 			data[j] = *(double *)dbuf;
 		}
-		for(j = 0; j < n_output_vars; j++) {
-			if(output_vars[j] >= 0) printf("%.14e", data[output_vars[j]]);
+		for (j = 0; j < n_output_vars; j++) {
+			if (output_vars[j] >= 0)
+				printf("%.14e", data[output_vars[j]]);
 			if (j < n_output_vars - 1) {
 				putchar(',');
 			} else {
